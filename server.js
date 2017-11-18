f53f30466a667c9884911ad1527d98e3b5e26986
John Elliott <johnelliott703@gmail.com>
2017-11-18 00:46:21 -0500

    Add thumbnails

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const debug = require('debug')('storycrop:server')
-const fs = require('fs')
+const debug = require('debug')('storycrops')
 const path = require('path')
 const express = require('express')
 const matt = require('./treatments/matt-story.js')
-const getJpegs = require('./lib/get-jpegs.js')
+const listThumbs = require('./lib/exifjson.js')
 
 require('dotenv').config()
+const CARD_PATH = process.env.CARD_PATH || '/Users/john/code/insta/fixtures/DCIM'
+const CROP_PATH = process.env.CROP_PATH || '/Users/john/code/story-crops/crops'
 
 var app = express()
 app.disable('x-powered-by')
@@ -16,19 +17,25 @@ app.set('view engine', 'pug')
 app.use('/:folder/:image', function (req, res, next) {
   debug('folder', req.params.folder)
   debug('image', req.params.image)
-  const imagePath = path.join('/media/card/DCIM/', req.params.folder, req.params.image)
-  const imageMattPath = path.join('/media/crops', req.params.image)
+  const imagePath = path.join(CARD_PATH, req.params.folder, req.params.image)
+  const imageMattPath = path.join(CROP_PATH, req.params.image)
   debug('maattt path', imageMattPath)
   matt(imagePath, imageMattPath).then(() => {
     res.redirect(`http://photon.local/crops/${req.params.image}`)
   })
 })
+
 app.use('/', function (req, res, next) {
-  getJpegs('/media/card/DCIM').then(list => {
-    const newlist = list.map(l => l.split('/').slice(4).join('/'))
-    debug(newlist instanceof Array)
-    res.render('list', { list: newlist })
-  })
+  listThumbs(CARD_PATH)
+    .then(list => {
+      const filtered = list.filter(l => l).map(l => {
+        return {
+          href: path.relative(CARD_PATH, l.SourceFile),
+          b64i: l.ThumbnailImage.slice(7)
+        }
+      })
+      res.render('list', { list: filtered })
+    })
 })
 
 // catch 404 and forward to error handler
