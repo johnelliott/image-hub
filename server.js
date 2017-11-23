e987edb8e5171fcb4bea0e771ad2bcf06036f819
John Elliott <johnelliott703@gmail.com>
2017-11-23 12:41:57 -0500

    Add db server endopoint

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const debug = require('debug')('storycrops')
+const debug = require('debug')('storycrop:server')
 const path = require('path')
 const express = require('express')
 const matt = require('./treatments/matt-story.js')
+const getJpegs = require('./lib/get-jpegs.js')
+const db = require('./lib/db.js')
 const listThumbs = require('./lib/get-thumbs.js')
 const formatBase64 = require('./lib/exiftool-b64-to-web-b64.js')
 
@@ -30,7 +32,7 @@ app.use('/:folder/:image', function (req, res, next) {
   })
 })
 
-app.use('/', function (req, res, next) {
+app.use('/gallery', function (req, res, next) {
   listThumbs(CARD_PATH)
     .then(list => {
       const filtered = list
@@ -43,6 +45,63 @@ app.use('/', function (req, res, next) {
         })
       res.render('list', { list: filtered })
     })
+})
+
+app.use('/posts', function (req, res, next) {
+  // const listFolders = fs.readdirSync(path.join('/media/card/DCIM/'))
+  const list = getJpegs('/media/card/DCIM')
+  // const list = listFolders.map(f => {
+  //   return fs.readdirSync(path.join(`/media/card/DCIM/${f}`))
+  //     .map(photoname => `${f}/${photoname}`)
+  //     .filter(fname => new RegExp(/jpg/, 'i').test(fname))
+  // })
+  debug('list', list)
+  res.render('list', { list })
+})
+
+/** image database api
+ * page lenght is 5
+ * query param is page
+ * resolves with array of SELECT * from image table
+ */
+app.use('/', function (req, res, next) {
+  debug('page', req.query.page)
+  const pageSize = 5
+  const offset = req.query.page ? ((parseInt(req.query.page, 10) - 1) * pageSize) : 0
+  debug('offset', offset)
+  db(path.join(__dirname, 'cam.db'))
+    .then(db => db.all(`SELECT * from image LIMIT ${pageSize} OFFSET ${offset}`, (err, result) => {
+      if (err) {
+        debug(err)
+        return res.status(404).end(err)
+      }
+      debug('db got', result)
+      result.map(i => {
+        return {
+          name: i.file_name,
+          dateTimeCreated: i.date_time_created,
+          thumbnail: i.thumbnail
+        }
+      })
+      // TODO support status codes for no content when we page out...
+      debug('WHAT MIME TYPE', req.headers)
+      // res.json(result)
+      return res.format({
+        'text/plain': function () {
+          res.render('list', { list })
+        },
+        'text/html': function () {
+          res.render('list', { list })
+        },
+        'application/json': function () {
+          res.json(result)
+        },
+        'default': function () {
+          // log the request and respond with 406
+          res.status(406).send('Not Acceptable')
+        }
+      })
+    }))
 })
 
 // catch 404 and forward to error handler
