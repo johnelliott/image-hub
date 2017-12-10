dc44c4de2806d650f281f293918ca5ffd7091fe9
John Elliott <johnelliott703@gmail.com>
2017-12-10 17:50:12 -0500

    Update image grid

--- client/image.js
+++ client/image.js
@@ -8,10 +8,5 @@ module.exports = function image (i, emit) {
   }
   return html`<li class="image-thumb">
     <a class="image-link" alt="small size" onclick=${view} href="/view/${i.name}"><img class="image-link" src="${i.b64i}"/></a>
-    <div class="image-thumb__crops">
-      <a class="image-thumb__icon" alt="full size" href="/storage/${i.name}">💯</a>
-      <a class="image-thumb__icon" alt="small size" href="/small/${i.name}">🖼</a>
-      <a class="image-thumb__icon" alt="instagram story" href="/stories/${i.name}">↕️</a>
-    </div>
   </li>`
 }
