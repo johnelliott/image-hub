22f9e7ae9022f6412c1fff665e40fc2bb89a8f2e
John Elliott <johnelliott703@gmail.com>
2017-12-29 09:54:05 -0500

    Fix display commands lint complaints

--- commands.js
+++ commands.js
@@ -1,17 +1,17 @@
+const debug = require('debug')('hub:commands')
 const cp = require('child_process')
 
 const SERVICE_NAMES = [
   'nginx.service',
   'image-hub-www.service',
-  'image-hub-etl.service',
+  'image-hub-etl.service'
 ]
 
-const cpu = "top -bn1 | grep load | awk '{printf \"%.2f\", $(NF-2)}'"
+const cpu = 'top -bn1 | grep load | awk \'{printf "%.2f", $(NF-2)}\''
 const memUsage = "free -m | awk 'NR==2{printf \"%s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-const ip = "hostname -I | cut -d\' \' -f1"
+const ip = 'hostname -I | cut -d\' \' -f1'
 const bigDisk = "df -h | awk '$NF==\"/\"{printf \"%d/%dGB %s\", $3,$2,$5}'"
-const disk = "df -h | awk '$NF==\"/\"{printf \"%s\", $5}'"
-const ssid = "iwgetid -r"
+const ssid = 'iwgetid -r'
 const services = `sudo systemctl list-units -t service --no-legend --no-pager -a ${SERVICE_NAMES.join(' ')} | tr -s ' ' | cut -d' ' -f1,3`
 
 module.exports = {
@@ -33,5 +33,5 @@ module.exports = {
       .map(s => `${s.name.slice(0, -8).split('-').pop()} ${s.status}`)
       .join(', ')
   },
-  memUsageSync: () => cp.execSync(memUsage),
+  memUsageSync: () => cp.execSync(memUsage)
 }
