7db87cc708b87116c33e8909f0c363bf95cc6e54
John Elliott <johnelliott703@gmail.com>
2017-12-29 10:33:25 -0500

    Add async display commands

--- commands.js
+++ commands.js
@@ -6,22 +6,59 @@ const SERVICE_NAMES = [
   'image-hub-www.service',
   'image-hub-etl.service'
 ]
+const CPU = 'top -bn1 | grep load | awk \'{printf "%.2f", $(NF-2)}\''
+const MEMUSAGE = "free -m | awk 'NR==2{printf \"%s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+const IP = 'hostname -I | cut -d\' \' -f1'
+const DISK = "df -h | awk '$NF==\"/\"{printf \"%d/%dGB %s\", $3,$2,$5}'"
+const SSID = 'iwgetid -r'
+const SERVICES = `sudo systemctl list-units -t service --no-legend --no-pager -a ${SERVICE_NAMES.join(' ')} | tr -s ' ' | cut -d' ' -f1,3`
 
-const cpu = 'top -bn1 | grep load | awk \'{printf "%.2f", $(NF-2)}\''
-const memUsage = "free -m | awk 'NR==2{printf \"%s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-const ip = 'hostname -I | cut -d\' \' -f1'
-const bigDisk = "df -h | awk '$NF==\"/\"{printf \"%d/%dGB %s\", $3,$2,$5}'"
-const ssid = 'iwgetid -r'
-const services = `sudo systemctl list-units -t service --no-legend --no-pager -a ${SERVICE_NAMES.join(' ')} | tr -s ' ' | cut -d' ' -f1,3`
+exports.cpuSync = () => cp.execSync(CPU),
+exports.diskSync = () => cp.execSync(DISK),
+exports.ipSync = () => cp.execSync(IP),
+exports.ssidSync = () => cp.execSync(SSID),
+exports.servicesSync = () => {
+  return cp.execSync(SERVICES, { encoding: 'utf8' })
+    .trim()
+    .split('\n').map(s => {
+      debug('services command closed')
+      const parts = s.split(' ')
+      return {
+        name: parts[0],
+        status: parts[1]
+      }
+    })
+    .filter(s => s.status !== 'active')
+    .map(s => `${s.name.slice(0, -8).split('-').pop()} ${s.status}`)
+    .join(', ')
+}
+exports.memUsageSync = () => cp.execSync(memUsage)
 
-module.exports = {
-  cpuSync: () => cp.execSync(cpu),
-  diskSync: () => cp.execSync(bigDisk),
-  ipSync: () => cp.execSync(ip),
-  ssidSync: () => cp.execSync(ssid),
-  servicesSync: () => {
-    return cp.execSync(services, { encoding: 'utf8' })
-      .trim()
+
+function commandPromiseWrapper (command = ssid, options = {}) {
+  debug('commandPromiseWrapper running')
+  return new Promise((resolve, reject) => {
+    debug('commandPromiseWrapper exec')
+    cp.exec(command, options, (err, result) => {
+      if (err) {
+        debug('commandPromiseWrapper reject')
+        return reject(err)
+      }
+      debug('commandPromiseWrapper resolve')
+      resolve(result)
+    })
+  })
+}
+
+exports.cpu = () => commandPromiseWrapper(CPU),
+exports.disk = () => commandPromiseWrapper(DISK)
+exports.ip = () => commandPromiseWrapper(IP)
+exports.ssid = () => commandPromiseWrapper(SSID)
+exports.services = () => {
+  return commandPromiseWrapper(SERVICES, { encoding: 'utf8' })
+    .then(services => {
+      debug('services command closed', services)
+      return services.trim()
       .split('\n').map(s => {
         const parts = s.split(' ')
         return {
@@ -32,6 +69,5 @@ module.exports = {
       .filter(s => s.status !== 'active')
       .map(s => `${s.name.slice(0, -8).split('-').pop()} ${s.status}`)
       .join(', ')
-  },
-  memUsageSync: () => cp.execSync(memUsage)
+    })
 }
