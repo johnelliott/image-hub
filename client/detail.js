54826ddd412fa0e31b2b5e2027f3f5d23b965e63
John Elliott <johnelliott703@gmail.com>
2018-01-14 17:16:51 -0500

    Update detail UI

--- client/detail.js
+++ client/detail.js
@@ -14,29 +14,28 @@ module.exports = function detail (state, emit) {
     emit('pushState', '/')
   }
   */
+  function endFlourish () {
+    return html`<a class="lightbox__tap-area" href="">#</a>`
+  }
+
   const nextImageLink = currentImageIndex + 1 < state.images.length
-    ? html`<a next class="lightbox__tap-area" href=/view/${state.images[currentImageIndex + 1].name}></a>`
-    : html`<a class="lightbox__tap-area" href=""></a>`
+    ? html`<a next class="lightbox__tap-area" href=/view/${state.images[currentImageIndex + 1].name}>→</a>`
+    : endFlourish()
   const previousImageLink = currentImageIndex > 0
-    ? html`<a prev class="lightbox__tap-area" href=/view/${state.images[currentImageIndex - 1].name}></a>`
-    : html`<a class="lightbox__tap-area" href=""></a>`
+    ? html`<a prev class="lightbox__tap-area" href=/view/${state.images[currentImageIndex - 1].name}>←</a>`
+    : endFlourish()
 
   return html`<body>
-    <nav><h1 class="heading-light"><a href="/" title="share"><span>Image Hub</span></a></h1></nav>
+    <nav><h1><a href="/" title="share"><span>Image Hub</span></a></h1></nav>
     <div class="lightbox">
       <div class="lightbox__content" >
-        <div class="lightbox__menu lightbox__menu--centered">
-          <span class="lightbox__text">${currentImageIndex + 1} of ${state.images.length}</span>
-        </div>
         <div class="lightbox__slide">
           <img class="lightbox-image" src="/small/${currentImage.name}"/>
-        </div>
-          <a alt="small size" href="/small/${currentImage.name}">
         <div class="lightbox__buttons">
           ${previousImageLink}
           ${nextImageLink}
         </div>
-          </a>
+        </div>
         <div class="lightbox__menu">
           <a class="lightbox__text" alt="instagram story" href="/stories/${currentImage.name}">ig-story</a>
           <a class="lightbox__text" alt="full size" href="/storage/${currentImage.name}">100%</a>
