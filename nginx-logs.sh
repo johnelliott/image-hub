e2835f28e3d422a1f7c4d4fe6fc625c4548ac766
John Elliott <johnelliott703@gmail.com>
2018-01-19 20:28:51 -0500

    Add log report scripts for systemd services

---
+++ nginx-logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Run this from macOS with ssh set up
+
+goaccess="/usr/local/bin/goaccess"
+command -v $goaccess >/dev/null 2>&1 || { echo >&2 "I require $goaccess but it's not installed"; exit 1; }
+
+if [ -z "$1" ]
+then
+  echo 'Need an ssh host as $1'
+  exit 0
+fi
+d=$(date +%Y-%m-%d_%H.%M.%S)
+report="access-log_$1_$d.html"
+echo creating log $report
+####### journalctl -u nginx.service -u image-hub-www.service -o cat --no-pager -b
+ssh $1 'journalctl -u nginx.service -o cat --no-pager' | $goaccess - -a --html-report-title="Nginx Stats $1" -o $report
+open $report
